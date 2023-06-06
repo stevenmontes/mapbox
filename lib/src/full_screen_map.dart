e2c7413197470644e4160b94bdfad096995e59b5
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-06-05 19:37:16 -0600

    310. Mapbox Studio - Cambiar el estilo de nuestro mapa

--- lib/src/full_screen_map.dart
+++ lib/src/full_screen_map.dart
@@ -11,7 +11,13 @@ class FullScreenMap extends StatefulWidget {
 class _FullScreenMapState extends State<FullScreenMap> {
   MapboxMapController? mapController;
 
-  var isLight = true;
+  final center = const LatLng(9.894701, -84.064173);
+  String selectedTheme =
+      'mapbox://styles/stevenmontes/clijlmlh400ti01pd73nt0u07';
+
+  final darkThemeMap = 'mapbox://styles/stevenmontes/clijlmlh400ti01pd73nt0u07';
+  final monochromeThemeMap =
+      'mapbox://styles/stevenmontes/clijlkb1s00sb01qfew8gdlp2';
 
   _onMapCreated(MapboxMapController controller) {
     mapController = controller;
@@ -20,12 +26,33 @@ class _FullScreenMapState extends State<FullScreenMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapboxMap(
-          accessToken:
-              'sk.eyJ1Ijoic3RldmVubW9udGVzIiwiYSI6ImNsaWpsMGoxbDA3ejYzZ3BoYjZyZjdmMXQifQ.lfbMcV4xuykZWF62EvIpGA',
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-              zoom: 14, target: LatLng(9.894701, -84.064173))),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              if (selectedTheme == darkThemeMap) {
+                selectedTheme = monochromeThemeMap;
+              } else {
+                selectedTheme = darkThemeMap;
+              }
+
+              setState(() {});
+            },
+            child: Icon(Icons.add_to_home_screen),
+          )
+        ],
+      ),
+      body: createMap(),
     );
+  }
+
+  MapboxMap createMap() {
+    return MapboxMap(
+        styleString: selectedTheme,
+        accessToken:
+            'sk.eyJ1Ijoic3RldmVubW9udGVzIiwiYSI6ImNsaWpsMGoxbDA3ejYzZ3BoYjZyZjdmMXQifQ.lfbMcV4xuykZWF62EvIpGA',
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(zoom: 14, target: center));
   }
 }
